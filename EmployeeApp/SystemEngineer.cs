3f6ce83e3857d3410682282c86bb77d8dc9c0c71
Bajahaw <rathi7137@gmail.com>
2024-04-06 16:52:02 +0300

    fixed SystemEngineer

--- EmployeeApp/SystemEngineer.cs
+++ EmployeeApp/SystemEngineer.cs
@@ -2,12 +2,13 @@ using System;
 using EmployeeApp.Interfaces;
 namespace EmployeeApp
 {
-    public class SystemEngineer : FullTimeEmployee, ISystemEngineer
+    public class SystemEngineer : CasualEmployee , ISystemEngineer
     {
         public string WorkPlace { get; set; }
         public SystemEngineer(int employeeId, string firstName, string lastName, 
-                            string email, string phone, Gender gender, Address address, double annualSalary)
-        : base(employeeId, firstName, lastName, email, phone, gender, address, annualSalary){ }
+                            string email, string phone, Gender gender, Address address,
+                            int hourlyPrice, int systemsDesigned, double bonus, double totalHours)
+        : base(employeeId, firstName, lastName, email, phone, gender, address, hourlyPrice, systemsDesigned, bonus, totalHours){ }
         public override string getJobName()
         {
             return "System Engineer";
