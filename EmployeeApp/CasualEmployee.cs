ee47edbf91439de5100f2c3241eb962abef77144
Bajahaw <rathi7137@gmail.com>
2024-04-06 17:33:00 +0300

    fixed var name

--- EmployeeApp/CasualEmployee.cs
+++ EmployeeApp/CasualEmployee.cs
@@ -3,22 +3,22 @@ namespace EmployeeApp
     public abstract class CasualEmployee : Employee
     {
         public double Bonus { get; set; }
-        public double SystemsDesigned { get; set; }
+        public double DoneTasks { get; set; }
         public double HourlyPrice { get; set; }
         public double TotalHours { get; set; }
         protected CasualEmployee(int employeeID, string firstName, string lastName, string email, 
                                     string phone, Gender gender, Address address, double hourlyPrice,
-                                    int systemsDesigned, double bonus, double totalHours)
+                                    int doneTasks, double bonus, double totalHours)
         : base(employeeID, firstName, lastName, email, phone, gender, address)
         {
-            SystemsDesigned = systemsDesigned;
+            DoneTasks = doneTasks;
             Bonus = bonus;
             HourlyPrice = hourlyPrice;
             TotalHours = totalHours;
         }
         public override double getMonthlySalary()
         {
-            return TotalHours * HourlyPrice + SystemsDesigned * Bonus;
+            return TotalHours * HourlyPrice + DoneTasks * Bonus;
         }
     }
 }
